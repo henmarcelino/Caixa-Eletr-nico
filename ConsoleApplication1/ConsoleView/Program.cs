167886f11b3464715d92a7f1d39ede57e14c359d
Aluno <Aluno@DOSO121799>
2018-06-23 11:22:35 -0300

    Comandos Básicos

---
+++ ConsoleApplication1/ConsoleView/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleView
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Console.WriteLine("Hello World");
+
+            i = int.Parse(Console.ReadLine());
+
+            Console.ReadKey();
+        }
+    }
+}
